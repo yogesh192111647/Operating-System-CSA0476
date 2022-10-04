704d03ad3e2e04bcc9738e08195ebf7fd65d6a84
yogesh192111647 <113873892+yogesh192111647@users.noreply.github.com>
2022-10-04 07:27:15 +0530

    DAY 1

---
+++ add two numbers .c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int number1,number2,sum;
+Printf("Enter two integers:");
+scanf("%d%d",&number1,&number2);
+sum=number1+number2;
+Printf(%d +%d = %d",number1,number2,sum);
+return 0;
+}
